6c1af873adc9c941e55fab0c27c0932f072a6610
Until-Code <djemal.dev@gmail.com>
2025-08-14 23:39:41 +0200

    feat(auth): add registration form and enable Bootstrap form theme

---
+++ src/Form/RegisterUserType.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RegisterUserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'label' => "Votre Adresse Email",
+                'attr' => [
+                    "placeholder" => "john@gmail.com"
+                ]
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => "Mot de passe",
+                'attr' => [
+                    "placeholder" => "Indiquez votre mot de passe"
+                ]
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => "Prénom",
+                'attr' => [
+                    "placeholder" => "Indiquez votre Prénom"
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => "Nom",
+                'attr' => [
+                    "placeholder" => "indiquez votre nom"
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => "Valider",
+                'attr' => [
+                    "class" => "btn btn-success"
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
