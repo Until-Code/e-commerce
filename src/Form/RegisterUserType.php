67d4afbbea8be4d0d90beaef4d6db25e9f00ef5f
Until-Code <djemal.dev@gmail.com>
2025-08-18 01:22:36 +0200

    feat(validation): enforce unique email and add length constraints on user entity fields

--- src/Form/RegisterUserType.php
+++ src/Form/RegisterUserType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\User;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -11,6 +12,7 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class RegisterUserType extends AbstractType
 {
@@ -23,11 +25,18 @@ class RegisterUserType extends AbstractType
                     "placeholder" => "john@gmail.com"
                 ]
             ])
-       
+
 
             ->add('plainPassword', RepeatedType::class,
             [
                 'type' => PasswordType::class,
+                'constraints' => [new Length([
+                    'min' => 3,
+                    'max' => 10,
+                    'minMessage'=> 'Veuillez saisir au minmum 3 caractère !',
+                    'maxMessage' => 'Veuillez saisir au maximum 10 caractères !'
+
+                ])],
                 'first_options'  => [
                     'attr' => [
                         "placeholder" => "Indiquez votre mot de passe"
@@ -42,12 +51,28 @@ class RegisterUserType extends AbstractType
             ])
 
             ->add('firstname', TextType::class, [
+
+                'constraints' => [new Length([
+                    'min' => 3,
+                    'max' => 10,
+                    'minMessage'=> 'Veuillez saisir au minmum 3 caractère !',
+                    'maxMessage' => 'Veuillez saisir au maximum 10 caractères !'
+
+                ])],
                 'label' => "Prénom",
                 'attr' => [
                     "placeholder" => "Indiquez votre Prénom"
                 ]
             ])
             ->add('lastname', TextType::class, [
+
+                'constraints' => [new Length([
+                    'min' => 3,
+                    'max' => 10,
+                    'minMessage'=> 'Veuillez saisir au minmum 3 caractère !',
+                    'maxMessage' => 'Veuillez saisir au maximum 10 caractères !'
+
+                ])],
                 'label' => "Nom",
                 'attr' => [
                     "placeholder" => "indiquez votre nom"
